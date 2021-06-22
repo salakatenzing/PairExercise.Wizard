157e3f0040687cb4f9fbbc87f01556d96048db84
Doug Henderson <dhend595@gmail.com>
2021-06-22 15:34:30 -0400

    Working on step 4

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
+
+
+
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+app.get('/', (req, res) =>{
+ const posts = postBank.list();
+ const webpage = `<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     <div>
+       <ul>
+          ${posts.map(post => ``)}
+       </ul>
+     </div>
+ </body>
+ </html>`
+})
+
+
+// ${posts.map(post => `<li>${"posts.title", 'posts.name'}</li>`)}
+// previous port was 1337.
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
